daf0cf5f81e69956ebc022a9709fe7a9fc5358fe
MichaelMaGG <michael2110@sina.com>
2021-09-17 09:00:03 +1200

    add register

--- src/components/access/Register.js
+++ src/components/access/Register.js
@@ -28,79 +28,105 @@ const validateMessages = {
 
   /* eslint-enable no-template-curly-in-string */
 
-const onFinish = (values) => {
-    console.log(values);
-};
+// const onFinish = (values) => {
+//     alert("hi finish");
+//     console.log(values);
+// };
   
 class Register extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {passStatus: false};
+        this.state = {
+            email: '',
+            password: '',
+            userName: ''
+        };
     
         // This binding is necessary to make `this` work in the callback
         this.submit = this.submit.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
     }
 
-    submit = () => {
-        axios.post('http://localhost:8090/user/add', {
-            userName: 'Fred',
-            password: 'Flintstone'
-        }).then(res => {
-            this.setState({ passStatus: res });
-            console.log("res:" + res);
-        }).catch(function (error) {
-            console.log(error);
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        console.log(name);
+    
+        this.setState({
+          [name]: value
+        });
+      }
+
+    submit = (event) => {
+        
+        // event.preventDefault();
+
+        alert("hi");
+
+        const post = {
+            email: this.state.email,
+            password: this.state.password,
+            userName: this.state.userName
+        }
+
+        
+        fetch('http://localhost:8090/account/add', {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(post),
+            })
+            .then(res => res.json())
+            .then(json => {
+                console.log(json)
         });
     }
 
     render() {
 		return (
-            <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
-                <Form.Item name={['user', 'name']} label="Name"
+            <Form {...layout} name="nest-messages" onFinish={this.submit} validateMessages={validateMessages}>
+                <Form.Item name={['user', 'email']} label="Email"
                     rules={[
                     {
                         required: true,
-                    },]}
+                        type: 'email',
+                    },
+                    ]}
                 >
-                    <Input />
+                    <Input name="email" onChange={this.handleInputChange}/>
                 </Form.Item>
                 <Form.Item name={['user', 'password']} label="Password"
                     rules={[
                     {
-                        type: 'email',
+                        required: true,
+                        type: 'string',
                     },
                     ]}
                 >
-                    <Input />
+                    <Input name="password" onChange={this.handleInputChange}/>
                 </Form.Item>
-                <Form.Item name={['user', 'email']} label="Email"
+                <Form.Item name={['user', 'confirmPassword']} label="Confirm password"
                     rules={[
                     {
-                        type: 'email',
+                        required: true,
+                        type: 'string',
                     },
                     ]}
                 >
-                    <Input />
+                    <Input name="confirmPassword" onChange={this.handleInputChange}/>
                 </Form.Item>
-                <Form.Item
-                    name={['user', 'age']}
-                    label="Age"
+                <Form.Item name={['user', 'name']} label="Name"
                     rules={[
                     {
-                        type: 'number',
-                        min: 0,
-                        max: 99,
-                    },
-                    ]}
+                        required: true,
+                        type: 'string',
+                    },]}
                 >
-                    <InputNumber />
-                </Form.Item>
-                <Form.Item name={['user', 'address']} label="Address">
-                    <Input />
-                </Form.Item>
-                <Form.Item name={['user', 'note']} label="Note">
-                    <Input.TextArea />
+                    <Input name="userName" onChange={this.handleInputChange} />
                 </Form.Item>
                 <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
                     <Button type="primary" htmlType="submit">
